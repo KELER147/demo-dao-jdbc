0f2da3c158bdc3a66a120c7b5e1bf506c455f2fa
Leonardo H. Keler <leonardo.keler.14@gmail.com>
2025-07-16 11:30:11 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,17 +13,11 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        SellerDao sellerdao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
 
-
-        System.out.println(obj);
         System.out.println(seller);
-
-
-
         sc.close();
     }
 }
